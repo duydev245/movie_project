8ef167847355956c9f63737633433090bad97325
Quasdas <110022635+Quasdas@users.noreply.github.com>
2024-01-24 20:25:38 +0700

    manh
    .

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
@@ -20,9 +20,28 @@ export class UserController {
     return this.userService.LayDanhSachLoaiNguoiDung();
   }
 
-
+  //API Lấy danh sách người dùng
+  @ApiTags('QuanLyNguoiDung')
   @Get('LayDanhSachNguoiDung')
+  @UseGuards(AuthGuard("jwt"))
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'Lấy danh sách người dùng thành công!!' })
+  @ApiResponse({ status: 400, description: 'Lấy danh sách người dùng thất bại !!' })
   LayDanhSachNguoiDung() {
     return this.userService.LayDanhSachNguoiDung()
+  }
+
+  @ApiTags('QuanLyNguoiDung')
+  @Get('LayDanhSachNguoiDungPhanTrang')
+  @UseGuards(AuthGuard("jwt"))
+  @ApiBearerAuth()
+  @ApiResponse({ status: 200, description: 'Lấy danh sách người dùng phân trang thành công!!' })
+  @ApiResponse({ status: 400, description: 'Lấy danh sách người dùng phân trang thất bại !!' })
+  LayDanhSachNguoiDungPhanTrang(
+    @Query('currentPage') currentPage: string,
+    @Query('limit') limit: string) {
+    const parsedCurrentPage = parseInt(currentPage, 10);
+    const parsedLimit = parseInt(limit, 10);
+    return this.userService.LayDanhSachNguoiDungPhanTrang(parsedCurrentPage, parsedLimit);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -10,21 +10,51 @@ export class UserService {
         try {
             const data = await this.prisma.nguoiDung.findMany({
                 select: {
-                  ho_ten: true,
-                  email: true,
-                  loai_nguoi_dung: true,
+                    ho_ten: true,
+                    email: true,
+                    loai_nguoi_dung: true,
                 },
-              });
-              return data;
+            });
+            return data;
         } catch (error) {
-            throw new HttpException({message:'Lay danh sach nguoi dung that bai'},HttpStatus.BAD_REQUEST)
+            throw new HttpException({ message: 'Lay danh sach nguoi dung that bai' }, HttpStatus.BAD_REQUEST)
         }
     }
 
     //API lấy danh sách người dùng
-    async LayDanhSachNguoiDung(){
+    async LayDanhSachNguoiDung() {
 
         const data = await this.prisma.nguoiDung.findMany()
         return data;
+    }
+
+    async LayDanhSachNguoiDungPhanTrang(currentPage: number, limit: number) {
+        try {
+            const totalItem = (await this.prisma.nguoiDung.findMany()).length;
+            const totalPages = Math.ceil(totalItem / limit)
+            if (currentPage < 1 || currentPage > totalPages) {
+                throw new Error("Invalid currentPage value");
+
+            }
+            console.log("Current Page:", currentPage);
+            console.log("Limit:", limit);
+            const offset = (currentPage - 1) * limit; // vị trí bắt đầu của bản ghi trong kết quả truy vấn
+            console.log("Offset:", offset);
+            const data = await this.prisma.nguoiDung.findMany({
+                take: limit,  // Số lượng bản ghi muốn lấy cho mỗi trang
+                skip: offset  // Vị trí bắt đầu của tập hợp con của dữ liệu
+            });
+
+            return {
+                data,
+                currentPage,
+                totalPages,
+                totalItem: totalItem,
+            }
+        } catch (error) {
+            console.log(error);
+            throw new HttpException({ message: "Lay danh sach nguoi dung failed!!" }, HttpStatus.BAD_REQUEST)
+        }
+
     }
 }
